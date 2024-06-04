3bf13ff8a2f67484417fad65603828bc55eec0fe
3disturbed <codemonkey@3disturbedmonkeys.co.uk>
2024-06-04 19:34:23 +0100

    Create userModel.js

---
+++ models/userModel.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcryptjs');
+const { v4: uuidv4 } = require('uuid');
+
+class User {
+    constructor(username, email, password, isConfirmed = false) {
+        this.id = uuidv4();
+        this.username = username;
+        this.email = email;
+        this.password = bcrypt.hashSync(password, 10);
+        this.isConfirmed = isConfirmed;
+    }
+
+    checkPassword(password) {
+        return bcrypt.compareSync(password, this.password);
+    }
+}
+
+module.exports = User;
