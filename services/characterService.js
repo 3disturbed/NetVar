66f6630295f46c810a23ab6bc8bcc10f53d47a5a
3disturbed <codemonkey@3disturbedmonkeys.co.uk>
2024-06-14 21:53:49 +0100

    Dark's Dev push.

--- services/characterService.js
+++ services/characterService.js
@@ -1,18 +1,32 @@
 const { getUsers } = require('../config/db');
+const bcrypt = require('bcryptjs');
 const Character = require('../models/characterModel');
-
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 let characters = [];
+var previousLength = 0;
+const filePath = './game/characters/characters.json';
+
 
 const loadCharacters = () => {
     // Load characters from a persistent storage if required
+    if (fs.existsSync(filePath)) {
+        const data = fs.readFileSync(filePath, 'utf8');
+        characters = JSON.parse(data);
+    } else {
+        console.log('No characters found. Creating new file.');
+        fs.writeFileSync(filePath, '[]', { encoding: 'utf8', flag: 'w' });
+    }
 };
 
 const saveCharacters = () => {
-    // Save characters to a persistent storage if required
+    const currentData = JSON.stringify(characters);
+    fs.writeFileSync(filePath, currentData, { encoding: 'utf8', flag: 'w' });
 };
 
-const getCharactersByAccountId = (accountId) => {
+const getCharactersByToken = (accountId) => {
     return characters.filter(character => character.accountId === accountId);
+    
 };
 
 const addCharacter = (character) => {
