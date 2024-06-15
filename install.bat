472bc8c6a70f68762778c0bb9f9c430a07eb1965
3disturbed <codemonkey@3disturbedmonkeys.co.uk>
2024-06-15 18:30:40 +0100

    Dark's Dev push.

--- install.bat
+++ install.bat
@@ -1,2 +1,12 @@
 @echo off
-npm install bcryptjs smtp-server jsonwebtoken body-parser
+echo Getting Data
+start npm install bcryptjs smtp-server jsonwebtoken body-parser
+git clone https://github.com/3disturbed/NetVar.git
+
+
+echo Press return after install to start servers.
+pause
+cd NetVar
+start Restart.bat
+
+exit
